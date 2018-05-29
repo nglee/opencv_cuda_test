e6e7aab4202d285965d62dd79ff66e410fecc85c
Namgoo Lee <lee.namgoo@sualab.com>
2018-05-29 10:06:06 +0900

    Update cuda_stream_master.cpp : Redesign test_2
    Now the test is more simple and focuses on the weak point.

--- cuda_stream_master/cuda_stream_master.cpp
+++ cuda_stream_master/cuda_stream_master.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 
-#define TEST_NUM        3               // Change test number
+#define TEST_NUM        2               // Change test number
 
 #define CONCAT(a, b)    a ## b
 #define CALL_TEST(n)    CONCAT(test_, n) ## ()
@@ -56,25 +56,23 @@ void test_2()
     Mat test1024x512 = imread("./test512x1024.png", IMREAD_GRAYSCALE); // This is an 512(col)x1024(row) image
     Mat test512x512T = imread("./test512x512T.png", IMREAD_GRAYSCALE); // This is the top half of the above image, so the size is 512(col)x512(row)
 
-    GpuMat d = pool.getBuffer(512, 512, CV_8UC1); // (1)
-    GpuMat d2 = d;
-    d = pool.getBuffer(512, 512, CV_8UC1);        // (2)
-    d.upload(test512x512T);
+    GpuMat d1 = pool.getBuffer(512, 512, CV_8UC1); // (1)
+    GpuMat d2 = pool.getBuffer(512, 512, CV_8UC1); // (2)
+    d2.upload(test512x512T);
 
-    d2.release();          // ***** Here we violate the deallocation rule when the StackAllocator is enabled. *****
-                           // ***** That is, the memory allocated on line (1) is deallocated before the memory allocated on line (2). *****
-                           // ***** In debug mode, the program stops on this line. In release mode, it doesn't, but let's see what happens. *****
+    d1.release();           // ***** Here we violate the deallocation rule when the StackAllocator is enabled. *****
+                            // ***** The memory allocated on line (1) is deallocated before the memory allocated on line (2). *****
+                            // ***** In debug mode, the program stops on this line. In release mode, it doesn't, but let's see what happens. *****
 
     Mat out;
-    d.download(out);
-    imshow("out", out);    // We expect to see test512x512T.png, and indeed, the expected image will be displayed.
-    waitKey(0);
+    d2.download(out);
+    imshow("out1", out);    // We expect to see test512x512T.png, and indeed, the expected image will be displayed.
 
     GpuMat d3 = pool.getBuffer(1024, 512, CV_8UC1);
     d3.upload(test1024x512);
 
-    d.download(out);
-    imshow("out", out);    // We expect to see test512x512T.png, but contrary to our expectation, the bottom half of test512x1024.png will be displayed.
+    d2.download(out);
+    imshow("out2", out);    // We expect to see test512x512T.png, but the bottom half of test512x1024.png will be displayed.
     waitKey(0);
 }
 
