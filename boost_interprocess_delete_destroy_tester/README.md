a5df06c9f935f96430973b3b370c7bc4c63230ca
Namgoo Lee <lee.namgoo@sualab.com>
2018-06-12 19:47:00 +0900

    boost interprocess delete destroy tester edited
    - modified to make it compatible with Visual Studio 2015
    
    - Note that regardless of using `managed_xsi_shared_memory` or
      `managed_windows_shared_memory`, deleting an `interprocess_mutex`
      while other process is holding it fails: it terminates the process.
    
    - IMPORTANT NOTE:
      For `managed_windows_shared_memory`, the shared memory itself is deleted
      when every process that uses it is gone.
      This is not true for `managed_xsi_shared_memory`. You need to delete it
      by yourself. This is the purpose of `deleter.cpp`.
      (Actually, this kind of behavior is kindly mentioned in the boost doc)

--- boost_interprocess_delete_destroy_tester/README.md
+++ boost_interprocess_delete_destroy_tester/README.md
@@ -1,3 +1,18 @@
+### What is this project?
+
+- To answer this question: https://stackoverflow.com/q/50691184/7724939
+
+- Note that regardless of using `managed_xsi_shared_memory` or
+  `managed_windows_shared_memory`, deleting an `interprocess_mutex`
+  while other process is holding it fails: it terminates the process.
+
+- IMPORTANT NOTE:
+  For `managed_windows_shared_memory`, the shared memory itself is deleted
+  when every process that uses it is gone.
+  This is not true for `managed_xsi_shared_memory`. You need to delete it
+  by yourself. This is the purpose of `deleter.cpp`.
+  (Actually, this kind of behavior is kindly mentioned in the [boost doc](https://www.boost.org/doc/libs/1_60_0/doc/html/interprocess/managed_memory_segments.html#interprocess.managed_memory_segments.managed_shared_memory.windows_managed_memory_common_shm))
+
 ### Requirement
 
 boost 1.60.0
--- boost_interprocess_delete_destroy_tester/boost_interprocess_delete_destroy_tester.cpp
+++ boost_interprocess_delete_destroy_tester/boost_interprocess_delete_destroy_tester.cpp
@@ -1,28 +1,42 @@
+
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+
+#ifdef _WIN32
+
+#include <boost/interprocess/managed_windows_shared_memory.hpp>
+typedef boost::interprocess::managed_windows_shared_memory smem_t;
+typedef int pid_t;
+#define getpid _getpid
+
+#else
+
 #include <boost/interprocess/xsi_key.hpp>
 #include <boost/interprocess/managed_xsi_shared_memory.hpp>
-#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+typedef boost::interprocess::managed_xsi_shared_memory smem_t
+
+#endif
 
 #include <iostream>
 
 using namespace boost::interprocess;
 
-void first_process(managed_xsi_shared_memory *shm)
+void first_process(smem_t *shm)
 {
     pid_t pid = ::getpid();
 
-    struct shm_remove
-    {
-        pid_t pid;
-        shm_remove(pid_t pid_) : pid(pid_) {}
-        ~shm_remove() {
-            try {
-                managed_xsi_shared_memory shm{ open_only, xsi_key("Makefile", 239) };
-                managed_xsi_shared_memory::remove(shm.get_shmid());
-            } catch (interprocess_exception& e) {
-                std::cout << std::to_string(pid) << ": (" << e.what() << ")" << std::endl;
-            }
-        }
-    } remover(pid);
+    //struct shm_remove
+    //{
+    //    pid_t pid;
+    //    shm_remove(pid_t pid_) : pid(pid_) {}
+    //    ~shm_remove() {
+    //        try {
+    //            smem_t shm{ open_only, xsi_key("Makefile", 239) };
+    //            smem_t::remove(shm.get_shmid());
+    //        } catch (interprocess_exception& e) {
+    //            std::cout << std::to_string(pid) << ": (" << e.what() << ")" << std::endl;
+    //        }
+    //    }
+    //} remover(pid);
 
     interprocess_mutex *mtx = shm->find_or_construct<interprocess_mutex>("gMutex")();
     interprocess_semaphore *sema1 = shm->find_or_construct<interprocess_semaphore>("gSema1")(0);
@@ -36,31 +50,35 @@ void first_process(managed_xsi_shared_memory *shm)
     sema2->post();
     std::cout << std::to_string(pid) << ": gSema2 posted, will delete sema1&2" << std::endl;
 
-    delete sema1;
-    delete sema2;
+    //delete sema1;
+    //delete sema2;
 }
 
-void second_process(managed_xsi_shared_memory *shm)
+void second_process(smem_t *shm)
 {
     pid_t pid = ::getpid();
 
-    struct shm_remove
-    {
-        pid_t pid;
-        shm_remove(pid_t pid_) : pid(pid_) {}
-        ~shm_remove() {
-            try {
-                managed_xsi_shared_memory shm{ open_only, xsi_key("Makefile", 239) };
-                managed_xsi_shared_memory::remove(shm.get_shmid());
-            } catch (interprocess_exception& e) {
-                std::cout << std::to_string(pid) << ": (" << e.what() << ")" << std::endl;
-            }
-        }
-    } remover(pid);
+    //struct shm_remove
+    //{
+    //    pid_t pid;
+    //    shm_remove(pid_t pid_) : pid(pid_) {}
+    //    ~shm_remove() {
+    //        try {
+    //            smem_t shm{ open_only, xsi_key("Makefile", 239) };
+    //            smem_t::remove(shm.get_shmid());
+    //        } catch (interprocess_exception& e) {
+    //            std::cout << std::to_string(pid) << ": (" << e.what() << ")" << std::endl;
+    //        }
+    //    }
+    //} remover(pid);
 
     try {
-        shm = new managed_xsi_shared_memory{ open_only, xsi_key("Makefile", 239) };
-    } catch (interprocess_exception& e) {
+#ifdef _WIN32
+        shm = new smem_t{ open_only, "boost_interprocess_delete_destroy_tester" };
+#else
+        shm = new smem_t{ open_only, xsi_key("Makefile", 239) };
+#endif
+    } catch (std::exception& e) {
         std::cout << std::to_string(pid) << ": shared memory open fail(" << e.what() << ")" << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -79,18 +97,22 @@ void second_process(managed_xsi_shared_memory *shm)
     mtx->unlock();
     std::cout << std::to_string(pid) << ": gMutex unlocked, will delete gMutex, gSema1&2" << std::endl;
 
-    delete mtx;
-    delete sema1;
-    delete sema2;
+    //delete mtx;
+    //delete sema1;
+    //delete sema2;
 }
 
 int main()
 {
     pid_t pid = ::getpid();
-    managed_xsi_shared_memory *shm;
+    smem_t *shm = nullptr;
 
     try {
-        shm = new managed_xsi_shared_memory{ create_only, xsi_key("Makefile", 239), 1024 };
+#ifdef _WIN32
+        shm = new smem_t{ create_only, "boost_interprocess_delete_destroy_tester", 1024 };
+#else
+        shm = new smem_t{ create_only, xsi_key("Makefile", 239), 1024 };
+#endif
         std::cout << std::to_string(pid) << ": shared memory created" << std::endl;
         first_process(shm);
     } catch (interprocess_exception& e) {
