3210ba2da330b157f92de8a0e1d3123da933ee7c
Namgoo Lee <namgoo.lee@cognex.com>
2020-04-06 11:14:24 +0900

    VS2019, boost 1.71.0 managed smem tester, cuda 10.2 matmul

---
+++ boost_interprocess_managed_smem_tester/boost_interprocess_managed_smem_tester.cpp
@@ -0,0 +1,18 @@
+﻿#include <boost/interprocess/managed_windows_shared_memory.hpp>
+#include <iostream>
+
+using namespace boost::interprocess;
+
+int main()
+{
+    managed_windows_shared_memory *shm = nullptr;
+
+    try {
+        shm = new managed_windows_shared_memory{ create_only, "shm", 1024 };
+        std::cout << "shared memory created" << std::endl;
+    } catch (interprocess_exception& e) {
+        std::cout << e.what() << std::endl;
+        shm = new managed_windows_shared_memory{ open_only, "shm" };
+        std::cout << "shared memory creation failed, opened instead" << std::endl;
+    }
+}
---
+++ cuda_matmul_10.2/kernel.cu
@@ -0,0 +1,81 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <random>
+#include <cstdlib>
+
+template <typename T>
+void _check(T result, char const* const func, char const* const file, int const line)
+{
+    if (result) {
+        fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line,
+            static_cast<unsigned int>(result), cudaGetErrorName(result), func);
+        cudaDeviceReset();
+        exit(EXIT_FAILURE);
+    }
+}
+#define checkCudaErrors(val) _check((val), #val, __FILE__, __LINE__)
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+__global__
+void matmul(float const* A, float const* B, float* C)
+{
+    
+}
+
+#define MATRIX_WIDTH (1ULL << 14)
+#define MATRIX_HEIGHT (1ULL << 14)
+#define MATRIX_SIZE (MATRIX_HEIGHT * MATRIX_WIDTH)
+#define MALLOC_SIZE (sizeof(float) * MATRIX_SIZE)
+
+int main()
+{
+    std::random_device rd;
+    std::mt19937 e2(rd());
+    std::uniform_real_distribution<float> dist(0, 1);
+
+    // Allocate memory
+    float* h_A = reinterpret_cast<float*>(std::malloc(MALLOC_SIZE));
+    float* h_B = reinterpret_cast<float*>(std::malloc(MALLOC_SIZE));
+    float* h_C = reinterpret_cast<float*>(std::malloc(MALLOC_SIZE));
+    float *d_A, *d_B, *d_C;
+    checkCudaErrors(cudaMalloc(&d_A, MALLOC_SIZE));
+    checkCudaErrors(cudaMalloc(&d_B, MALLOC_SIZE));
+    checkCudaErrors(cudaMalloc(&d_C, MALLOC_SIZE));
+
+    // Init memory
+    for (int idx = 0; idx < MATRIX_SIZE; idx++) {
+        h_A[idx] = dist(e2);
+        h_B[idx] = dist(e2);
+    }
+
+    // Copy host to device
+    checkCudaErrors(cudaMemcpy(d_A, h_A, MALLOC_SIZE, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_B, h_B, MALLOC_SIZE, cudaMemcpyHostToDevice));
+
+    dim3 gridSize(1024);
+    dim3 blockSize(1024);
+
+    matmul << <gridSize, blockSize >> > (d_A, d_B, d_C);
+
+    // Copy device to host
+    checkCudaErrors(cudaMemcpy(h_C, d_C, MALLOC_SIZE, cudaMemcpyDeviceToHost));
+
+    // Free memory
+    checkCudaErrors(cudaFree(d_C));
+    checkCudaErrors(cudaFree(d_B));
+    checkCudaErrors(cudaFree(d_A));
+    free(h_C);
+    free(h_B);
+    free(h_A);
+}
+
